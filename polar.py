06e985dc5302c03d03f2736e49abec26b7972a4f
mikolalysenko <mikolalysenko@gmail.com>
2010-03-30 14:46:16 -0500

    Moved polar fft stuff into polar, renamed things

--- polar.py
+++ polar.py
@@ -1,5 +1,9 @@
-from math import atan2;
-from scipy import *
+from math import cos, sin, exp, sqrt, atan2;
+from scipy import conjugate, real, array;
+from scipy.linalg.basic import norm;
+from scipy.fftpack.basic import fft2, ifft2;
+from scipy.fftpack.helper import fftshift, ifftshift;
+from scipy.fftpack.pseudo_diffs import shift;
 from scipy.interpolate import interp1d;
 
 __theta_samples = [ array([0]), array([0.*pi/8.,1.*pi/8.,2.*pi/8.,3.*pi/8.,4.*pi/8.,5.*pi/8.,6.*pi/8.,7.*pi/8.])]
@@ -246,4 +250,61 @@ def polar2rect( fp, xs, ys ):
 				f[x0-x,y0+y] = res[4*p-t+4]
 				t = t + 1
 	return f
+
+
+
+#Converts geometry into polar fourier transform
+def pfft(real_geom, nR):
+	return rect2polar(ifftshift(fft2(ifftshift(__cpad(real_geom)))), nR);
+
+#Converts polar fourier transform into geometry
+def ipfft(pft, xs, ys):
+	return fftshift(real(ifft2(fftshift(polar2rect(pft, xs, ys)))))
+
+
+#Pads geometry so that it doesn't have overlap issues
+def __cpad(f):
+	ns = int(round(norm(array(f.shape))));
+	res = zeros((ns,ns));
+	c0 = array((ns / 2. - array(f.shape)/2.).round(), dtype('int'));
+	c1 = c0 + array(f.shape, dtype('int'));
+	res[c0[0]:c1[0], c0[1]:c1[1]] = f;
+	return res;
+
+#Performs pointwise polynomial multiplication to evaluate a single point
+# Point coordinates are given in polar form
+def eval_pft(pft, p, phi):
+	res = pft[0][0];	
+	m = -2.j * pi / (len(pft));
+	for r in range(1, len(pft)):
+		c = pft[r];
+		for t in range(len(c)):
+			theta = t * 2*pi / len(c);
+			v = exp(m * p * r * cos(theta - phi));
+			#print cc, ss, v;
+			res += c[t] * v;
+	return res / (len(pft));
+	
+#Performs a convolution
+def pft_mult(pX, pY):
+	res = []
+	for k in range(len(pX)):
+		res.append(pX[k] * conjugate(pY[k]));
+	return res;
+
+
+#Rotates some polar geometry
+def pft_rotate(pft, theta):
+	R = len(pft);
+	res = [];
+	for k in range(R):
+		res.append(shift(pft[k], theta));
+	return res;
+
+def pft_scale(pft, s):
+	assert(False);
+	
+def pft_shift(pft, x, y):
+	assert(False);
+
 
