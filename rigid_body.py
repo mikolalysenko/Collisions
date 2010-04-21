501606ab20f782805236353e6cdff06259ef7d2c
mikola <mikola@sal-0019.(none)>
2010-04-21 13:42:52 -0500

    Checking in local changes

--- rigid_body.py
+++ rigid_body.py
@@ -2,10 +2,9 @@
 Rigid body dynamics for python
 '''
 from scipy import matrix, array, eye
+from shapes import *
 from obstacle import *
 from se2 import *
-
-
 
 
 
@@ -22,7 +21,16 @@ class RigidBody:
 
 class RigidBodySystem:
 	bodies = []
+	gravity = -10.
 
+	def __init__(self):
+		self.bodies = []
+		self.gravity = -10.
+	
 	def add_body(self, body):
 		bodies.append(body)
-		
+	
+	def integrate(self, dt):
+		return 0
+
+
---
+++ visualize.py
@@ -0,0 +1,45 @@
+from os import *
+from sys import *
+from scipy import *
+from numpy import *
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import shapes
+import obstacle
+import rigid_body
+
+
+class Visualization
+	
+	system = None
+
+	def __init__(self, system):
+		self.system = system
+		self.screen = array([800,600])
+		
+		#Initialize pygame
+		pygame.init()
+		pygame.display.set_caption('Dynamics Visualizer')
+		pygame.display.set_mode(self.screen,OPENGL|DOUBLEBUF)
+		
+		#Initialize opengl
+		GL.resize(self.screen)
+		GL.init()
+
+	def update(self):
+		return 0
+
+	def draw(self):
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+		pygame.display.flip()
+
+	def loop(self):
+		while(True):
+			
+			system.integrate(0.1)
+			self.draw()
+			
+
+
